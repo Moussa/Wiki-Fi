dde6e86c8d25bb75c6d69cee49e423f40c88f3e2
Moussekateer <moussa.uk@gmail.com>
2013-04-11 22:15:46 +0100

    add google analytics

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,3 +1,4 @@
+// GoSquared
 var GoSquared = {};
 GoSquared.acct = "GSN-064561-T";
 (function(w){
@@ -13,3 +14,14 @@ GoSquared.acct = "GSN-064561-T";
     w.addEventListener("load", gs, false) :
     w.attachEvent("onload", gs);
 })(window);
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40084408-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
